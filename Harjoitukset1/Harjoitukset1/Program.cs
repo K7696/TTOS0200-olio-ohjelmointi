f90aa432cfa0673e8a8f2fef040586f4a327d5af
k7696 <k7696@jamk.fi>
2017-01-09 19:22:42 +0200

    Harjoitus 1 tehty

--- Harjoitukset1/Harjoitukset1/Program.cs
+++ Harjoitukset1/Harjoitukset1/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Harjoitukset1
 {
+    public enum Luvut
+    {
+        Yksi = 1,
+        Kaksi = 2,
+        Kolme = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int luku;
+
+            Console.Write("Anna luku > ");
+
+            while (!int.TryParse(Console.ReadLine(), out luku))
+            {
+                Console.Write("Anna luku > ");
+            }
+
+            StringBuilder message = new StringBuilder();
+
+            if(luku == (int)Luvut.Yksi)
+            {
+                message.AppendFormat("Annoit luvun {0}", Luvut.Yksi);
+            }
+            else if (luku == (int)Luvut.Kaksi)
+            {
+                message.AppendFormat("Annoit luvun {0}", Luvut.Kaksi);
+            }
+            else if (luku == (int)Luvut.Kolme)
+            {
+                message.AppendFormat("Annoit luvun {0}", Luvut.Kolme);
+            }
+            else
+            {
+                message.Append("Annoit jonku muun luvun");
+            }
+
+            Console.WriteLine(message.ToString());
+
+            Console.ReadKey();
         }
     }
 }

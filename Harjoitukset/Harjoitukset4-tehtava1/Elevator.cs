c8ba7aca508a3b20b48a12cba00b8795fb380103
Reiska <pasi.reini@outlook.com>
2017-02-06 08:05:03 +0200

    Harjoitukset4

---
+++ Harjoitukset/Harjoitukset4-tehtava1/Elevator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harjoitukset4_tehtava1
+{
+    public class Elevator
+    {
+        #region Fields
+
+        private int minFloor = 1;
+        private int maxFloor = 5;
+        private int floor = 0;
+        private bool run = true;
+
+        #endregion Fields
+
+        #region Properties
+
+        public int Floor {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value < minFloor)
+                {
+                    Console.WriteLine("Floor is too small!");
+                }
+                else if(value > maxFloor)
+                {
+                    Console.WriteLine("Floor is too big!");
+                }
+                else {
+                    floor = value;
+                    moveElevator();
+                }
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Default ctor
+        /// </summary>
+        public Elevator()
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Private methods
+
+        private void moveElevator()
+        {
+            Console.WriteLine(string.Format("Elevator is now in floor: {0}", floor));
+        }
+
+        #endregion Private methods
+
+        #region Public methods
+
+        public void AskFloor()
+        {
+            Console.WriteLine("Give a new floor number (1-5)");
+        }
+
+        #endregion Public methods
+    }
+}
---
+++ Harjoitukset/Harjoitukset4-tehtava1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harjoitukset4_tehtava1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             * Tehtävänäsi on ohjelmoida Dynamon hissin kerroksen ohjaukseen liittyvä säädin. 
+             * Toteutetun ohjelman tulee pystyä kysymään käyttäjältä haluttu kerros ja siirtämään 
+             * hissi haluttuun kerrokseen (tässä tapauksessa ohjelma kertoo käyttäjälle missä 
+             * kerroksessa hissi on). Muista, että Dynamon hissi voi olla vain kerroksissa 1-5. 
+             * Käytä Hissi-luokassa get- ja set-aksessoreita suojamaan olion tila.Tehtävänäsi on 
+             * ohjelmoida Dynamon hissin kerroksen ohjaukseen liittyvä säädin. Toteutetun ohjelman 
+             * tulee pystyä kysymään käyttäjältä haluttu kerros ja siirtämään hissi haluttuun 
+             * kerrokseen (tässä tapauksessa ohjelma kertoo käyttäjälle missä kerroksessa hissi on). 
+             * Muista, että Dynamon hissi voi olla vain kerroksissa 1-5. Käytä Hissi-luokassa get- ja 
+             * set-aksessoreita suojamaan olion tila.
+             */
+
+            // Luo instanssi
+            Elevator elevator = new Elevator();
+
+            int floor = 0;
+            bool canContinue = false;
+
+            while (canContinue == false)
+            {
+                elevator.AskFloor();
+                canContinue = int.TryParse(Console.ReadLine(), out floor);
+
+                elevator.Floor = floor;
+                canContinue = false;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
